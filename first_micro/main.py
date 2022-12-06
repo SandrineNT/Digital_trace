3bc6fc98f331f4fd7b6f8c8d857347ab8f1b79c9
SandrineNT <sandrine.nguemkamtchuinkam@epfedu.fr>
2022-12-06 22:57:54 +1000

    sixth commit

--- first_micro/main.py
+++ first_micro/main.py
@@ -4,4 +4,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-250994065-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-250994065-2');
+</script>
+ """
+ return prefix_google + "Hello World"
