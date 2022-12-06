4d7c2a15946cac39ab817691bf1c9f294cba2dbb
SandrineNT <sandrine.nguemkamtchuinkam@epfedu.fr>
2022-12-06 12:26:20 +0100

    12 commit

--- first_micro/main.py
+++ first_micro/main.py
@@ -12,14 +12,7 @@ src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', ' <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-250994065-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-250994065-2');
+ gtag('config', 'UA-250994065-2');
 </script>
  """
  return prefix_google + "Hello World"
